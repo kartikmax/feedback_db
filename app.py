a31ea9efad128e7a8c6c276c1cfbe0ada1452364
kartikmax <kyadav5213@gmail.com>
2023-03-12 20:32:46 +0530

    here is the env file

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ def submit():
             data = Feedback(customer, dealer, rating, comments)
             db.session.add(data)
             db.session.commit()
-            # send_mail(customer, dealer, rating, comments)
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback')
 
